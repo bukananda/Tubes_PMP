7200e379e2cc4ad3980787ffd4dc34f1d267e086
Berwyn L <151898586+rikorealm@users.noreply.github.com>
2025-06-24 21:49:44 +0700

    Delete include directory

--- include/pengelolaanData.h
+++
@@ -1,9 +0,0 @@
-#ifndef PENGELOLAANDATA_H
-#define PENGELOLAANDATA_H
-
-void tambahDataDokter(const char *file);
-void tampilkanSemua(const char *file);
-void tampilkanFilter(const char *file);
-void hapusDataDokter(const char *file);
-
-#endif
